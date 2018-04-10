78daaec4df319e39f67020206794414231cac561
nguyen.van.dat <nguyen.van.dat@alliedtechbase.com>
2018-04-10 17:33:52 +0700

    update check ip range

--- config/base.php
+++ config/base.php
@@ -2,4 +2,6 @@
 
 return [
     'default_password' => env('DEFAULT_PASSWORD', '123456789'),
+    'domain'           => env('CONFIG_DOMAIN'),
+    'ip_range'         => explode(',', env('CONFIG_IP_RANGE_AA_NETWORK')),
 ];
