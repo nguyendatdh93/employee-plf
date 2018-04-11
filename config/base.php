0c9626aa8f92e34b7bc11b0f7d5dedb4069a8658
nguyen.van.dat <nguyen.van.dat@alliedtechbase.com>
2018-04-11 15:13:58 +0700

    move storage save log in env

--- config/base.php
+++ config/base.php
@@ -2,6 +2,5 @@
 
 return [
     'default_password' => env('DEFAULT_PASSWORD', '123456789'),
-    'domain'           => env('CONFIG_DOMAIN'),
     'ip_range'         => explode(',', env('CONFIG_IP_RANGE_AA_NETWORK')),
 ];
