38fe11d572481e0f1ca4bfb0998a7089e8e8e2e1
atb_nguyentienphuoc <nguyen.tien.phuoc@alliedtechbase.com>
2018-04-12 14:27:18 +0700

    fix height user management page

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,6 +13,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
         <!-- Main Header -->
         @include('layouts.partials.main_header')
+
         <!-- Left side column. contains the logo and sidebar -->
         @include('layouts.partials.main_sidebar')
 
@@ -20,7 +21,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         @include('layouts.partials.content_wrapper')
 
         <!-- Main Footer -->
-            @include('layouts.partials.main_footer')
+        @include('layouts.partials.main_footer')
 
     </div><!-- ./wrapper -->
 @show
