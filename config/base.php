227ddbabb2cafda35e6fa3e2d73cec92ec37a787
atb_nguyentienphuoc <nguyen.tien.phuoc@alliedtechbase.com>
2018-04-12 09:58:58 +0700

    - edit mail forgot password - create admin account when migrate

--- config/base.php
+++ config/base.php
@@ -4,4 +4,7 @@ return [
     'default_password' => env('DEFAULT_PASSWORD', '123456789'),
     'ip_range'         => explode(',', env('CONFIG_IP_RANGE_AA_NETWORK')),
     'copy_right_text'  => env('COPYRIGHT_TEXT_IN_FOOTER', ''),
+    'manager_name'     => env('MANAGER_NAME', 'Manager'),
+    'manager_email'    => env('MANAGER_EMAIL', 'manager@aainc.co.jp'),
+    'manager_password' => env('MANAGER_PASSWORD', '123456789'),
 ];
