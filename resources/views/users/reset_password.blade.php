31a9c9eb3d6f0ab5d17206d61ce6629fa7ace482
nguyen.van.dat <nguyen.van.dat@alliedtechbase.com>
2018-04-16 10:44:08 +0700

    add multi lang to reset pass page

--- resources/views/users/reset_password.blade.php
+++ resources/views/users/reset_password.blade.php
@@ -17,27 +17,27 @@
             </div>
         @endif
         <div class="register-box-body">
-            <h4 class="login-box-msg">You must change your password on the frist time.</h4>
+            <h4 class="login-box-msg">{{ __('reset_password.title_reset_password') }}</h4>
 
             <form action="{{ route('reset_password') }}" method="post">
                 {{ csrf_field() }}
                 <div class="box-body">
                     <input type="hidden" name="reset_password" value="1" />
                     <div class="form-group new-password @if (Session::get('error_new_password')) has-error @endif">
-                        <label for="exampleInputPassword1">{{ __('change_password.new_password') }}</label>
-                        <input type="password" name="new_password" class="form-control" id="exampleInputPassword1" placeholder="{{ __('change_password.new_password') }}">
+                        <label for="exampleInputPassword1">{{ __('reset_password.new_password') }}</label>
+                        <input type="password" name="new_password" class="form-control" id="exampleInputPassword1" placeholder="{{ __('reset_password.new_password') }}">
                         <span class="help-block">@if (Session::get('error_new_password')) {{ Session::get('error_new_password') }} @endif</span>
                     </div>
                     <div class="form-group confirm-new-password @if (Session::get('error_confirm_new_password')) has-error @endif" >
-                        <label for="exampleInputPassword1">{{ __('change_password.confirm_new_password') }}</label>
-                        <input type="password" name="confirm_new_password" class="form-control" id="exampleInputPassword1" placeholder="{{ __('change_password.confirm_new_password') }}">
+                        <label for="exampleInputPassword1">{{ __('reset_password.confirm_new_password') }}</label>
+                        <input type="password" name="confirm_new_password" class="form-control" id="exampleInputPassword1" placeholder="{{ __('reset_password.confirm_new_password') }}">
                         <span class="help-block"> @if (Session::get('error_confirm_new_password')) {{ Session::get('error_confirm_new_password') }} @endif</span>
                     </div>
                 </div>
                 <!-- /.box-body -->
 
                 <div class="box-footer" style="text-align: center;">
-                    <button type="submit" class="btn btn-primary btn-change-password">{{ __('change_password.btn_change_password') }}</button>
+                    <button type="submit" class="btn btn-primary btn-change-password">{{ __('reset_password.btn_change_password') }}</button>
                 </div>
             </form>
         </div>
