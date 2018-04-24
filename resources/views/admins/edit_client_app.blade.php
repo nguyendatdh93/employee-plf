c36214b835398e7917a957cf58d81f0da902772c
nguyen.van.dat <nguyen.van.dat@alliedtechbase.com>
2018-04-24 11:56:04 +0700

    update hold old data for edit f

--- resources/views/admins/edit_client_app.blade.php
+++ resources/views/admins/edit_client_app.blade.php
@@ -29,21 +29,21 @@
                         <div class="form-group client-name @if ($errors->has('client_name')) has-error @endif">
                             <label for="exampleInputEmail1" class="col-sm-2 control-label">{{ __('edit_client_app.client_name') }} <span class="required">*</span></label>
                             <div class="col-sm-10">
-                                <input type="text" value="{{ $oauth_client[0]->name }}" name="client_name" class="form-control" id="exampleInputPassword1" placeholder="" required autofocus>
+                                <input type="text" value="{{ old('client_name', $oauth_client[0]->name) }}" name="client_name" class="form-control" id="exampleInputPassword1" placeholder="" required autofocus>
                                 <span class="help-block"> @if ($errors->has('client_name')) {{ $errors->first('client_name') }} @endif</span>
                             </div>
                         </div>
                         <div class="form-group url-redirect @if ($errors->has('url_redirect')) has-error @endif">
                             <label for="exampleInputPassword1" class="col-sm-2 control-label">{{ __('edit_client_app.client_call_back') }} <span class="required">*</span></label>
                             <div class="col-sm-10">
-                                <input type="text" value="{{ $oauth_client[0]->redirect }}" name="url_redirect" class="form-control" id="exampleInputPassword1" placeholder="" required autofocus>
+                                <input type="text" value="{{ old('url_redirect', $oauth_client[0]->redirect) }}" name="url_redirect" class="form-control" id="exampleInputPassword1" placeholder="" required autofocus>
                                 <span class="help-block">@if ($errors->has('url_redirect')) {{ $errors->first('url_redirect') }} @endif</span>
                             </div>
                         </div>
                         <div class="form-group ip-secure @if ($errors->has('ip_secure')) has-error @endif" >
                             <label for="exampleInputPassword1" class="col-sm-2 control-label">{{ __('edit_client_app.ip_secure') }}</label>
-                            <div class="col-sm-6">
-                                <input type="text" value="{{ $oauth_client[0]->ip_secure }}" name="ip_secure" class="form-control" id="exampleInputPassword1" placeholder="">
+                            <div class="col-sm-10">
+                                <input type="text" value="{{ old('ip_secure', $oauth_client[0]->ip_secure) }}" name="ip_secure" class="form-control" id="exampleInputPassword1" placeholder="">
                                 <span class="help-block"> @if ($errors->has('ip_secure')) {{ $errors->first('ip_secure') }} @endif</span>
                             </div>
                         </div>
