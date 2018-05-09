af046a84d77e04c4a28fa31038d5329968529248
nguyen.van.dat <nguyen.van.dat@alliedtechbase.com>
2018-05-09 17:03:11 +0700

    update button for mail template forgot password

--- resources/views/mail/forgot_password_body.blade.php
+++ resources/views/mail/forgot_password_body.blade.php
@@ -2,7 +2,17 @@
 
 社員認証システムのパスワードリマインダーが使用されました。<br><br>
 
-<a href="{{ $url }}">パスワード リセット ボタン</a><br><br>
+<a href="{{ $url }}" style="
+    color: white;
+    padding: 5px 9px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    border-radius: 3px;
+    border-width: 1px;
+    background-color: #3c8dbc;
+    border-color: #367fa9;
+    margin-left: 50px;">パスワードリセット</a><br><br>
 
 また、身に覚えが無い場合は、下記よりお問い合わせください。<br><br>
 
